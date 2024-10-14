9b9ef9d3d1a2c332a06bffddfcb26ed90f2910ef
coderwizard <tyagiparth286@gmail.com>
2024-10-14 11:01:07 +0530

    Second Commit

---
+++ src/lib/utils.ts
@@ -0,0 +1,45 @@
+// IT APPEARS THAT BIG CALENDAR SHOWS THE LAST WEEK WHEN THE CURRENT DAY IS A WEEKEND.
+// FOR THIS REASON WE'LL GET THE LAST WEEK AS THE REFERENCE WEEK.
+// IN THE TUTORIAL WE'RE TAKING THE NEXT WEEK AS THE REFERENCE WEEK.
+
+const getLatestMonday = (): Date => {
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const daysSinceMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+  const latestMonday = today;
+  latestMonday.setDate(today.getDate() - daysSinceMonday);
+  return latestMonday;
+};
+
+export const adjustScheduleToCurrentWeek = (
+  lessons: { title: string; start: Date; end: Date }[]
+): { title: string; start: Date; end: Date }[] => {
+  const latestMonday = getLatestMonday();
+
+  return lessons.map((lesson) => {
+    const lessonDayOfWeek = lesson.start.getDay();
+
+    const daysFromMonday = lessonDayOfWeek === 0 ? 6 : lessonDayOfWeek - 1;
+
+    const adjustedStartDate = new Date(latestMonday);
+
+    adjustedStartDate.setDate(latestMonday.getDate() + daysFromMonday);
+    adjustedStartDate.setHours(
+      lesson.start.getHours(),
+      lesson.start.getMinutes(),
+      lesson.start.getSeconds()
+    );
+    const adjustedEndDate = new Date(adjustedStartDate);
+    adjustedEndDate.setHours(
+      lesson.end.getHours(),
+      lesson.end.getMinutes(),
+      lesson.end.getSeconds()
+    );
+
+    return {
+      title: lesson.title,
+      start: adjustedStartDate,
+      end: adjustedEndDate,
+    };
+  });
+};
